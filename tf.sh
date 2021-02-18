ecafa8485c2c3f83d91df031385c92b6646fe9a6
Forrest Loomis <forrest.loomis@ripcitysoftware.com>
2021-02-18 14:48:17 -0500

    update response test
    - add validation URL printing

--- tf.sh
+++ tf.sh
@@ -17,15 +17,14 @@ validate_plan() {
     URL="${CM_VALIDATION_API}/cloudpolicies/${CM_PROJECT_ID}"
     terraform show -json plan.tfplan | jq '. | {CI_COMMIT_SHA: env.CI_COMMIT_SHA, CI_COMMIT_SHORT_SHA: env.CI_COMMIT_SHORT_SHA, CI_COMMIT_TITLE: env.CI_COMMIT_TITLE, CI_JOB_URL: env.CI_JOB_URL, CI_PIPELINE_IID: env.CI_PIPELINE_IID, CI_PIPELINE_URL: env.CI_PIPELINE_URL, CI_PROJECT_ID: env.CI_PROJECT_ID, GITLAB_USER_EMAIL: env.GITLAB_USER_EMAIL, GITLAB_USER_LOGIN: env.GITLAB_USER_LOGIN, GITLAB_USER_NAME: env.GITLAB_USER_NAME, CM_PROJECT_ID: env.CM_PROJECT_ID, CM_ORG_ID: env.CM_ORG_ID, CM_ENVIRONMENT_NAME: env.ENV, CM_STATE: .}' >request.json
     RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d @request.json $URL 2>/dev/null)
-    test $(echo $RESPONSE | jq '.summary.valid') = 'true'
+    test $(echo $RESPONSE | jq '.passed') = 'true'
 
     if [ ! $? -eq 0 ]; then
         echo -e "\n\nYour CloudPolicy validation failed!\n\n"
-        # TODO: Reenable a validation URL as soon as this is working
-        # echo -e "For more details or to request a resolution visit:\n"
-        # VALIDATION_URL="$(echo $RESPONSE | jq '.validation_url')"
-        # echo -e "\n\t$VALIDATION_URL"
-        # exit 1
+        echo -e "For more details or to request a resolution visit:\n"
+        VALIDATION_URL="$(echo $RESPONSE | jq '.validation_url')"
+        echo -e "\n\t$VALIDATION_URL"
+        exit 1
     fi
 
     echo -e "\n\nYour planned changes pass CloudPolicy validation!\n"
